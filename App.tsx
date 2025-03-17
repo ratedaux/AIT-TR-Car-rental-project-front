075890b338ab5d35336fd5b6787fb7956a9643f7
ElenaKars <elena_baldina@yahoo.com>
2025-03-17 17:09:54 +0100

    layout commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './src/styles/globalStyle.css';
+import Layout from 'pages/carRentalApp/Layout/Layout';
+import Home from 'pages/carRentalApp/Home/Home';
 
 
 const App = () => {
   return (
     <BrowserRouter>
-    Car Rental Project
-    </BrowserRouter>
-  )
-}
+      <Layout>
+        <Home />
+      </Layout>
 
-export default App
+
+    </BrowserRouter>
+  );
+};
+
+export default App;
