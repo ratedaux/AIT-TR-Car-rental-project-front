5239d7c9e1df436900b6001de15f88d34daed2f5
Neshyna <mariya.neshyna@gmail.com>
2025-03-24 11:17:05 +0100

    edit car form

---
+++ src/components/EditCarForm/types.ts
@@ -0,0 +1,11 @@
+export interface EditCarFormProps{
+    brand: string
+    model: string
+    status:string
+    year: number
+    bodyType: string
+    fuelType: string
+    transmissionType: string
+    dayRentalPrice: number
+    carImage?: string
+}
