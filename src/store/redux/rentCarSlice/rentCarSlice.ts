3c693d4069ddf2894c0dc334022c21194b4b4d65
ElenaKars <elena_baldina@yahoo.com>
2025-03-28 13:05:22 +0100

    rentCarSlice

--- src/store/redux/rentCarSlice/rentCarSlice.ts
+++ src/store/redux/rentCarSlice/rentCarSlice.ts
@@ -1,36 +1,113 @@
 import { createAppSlice } from "store/createAppSlice"
-import { rentCarSliceState } from "./types"
+import { brandsSliceState, Car, rentCarSliceState } from "./types"
 import axios from "axios"
 
-const rentCarState: rentCarSliceState = {
+const initialCarState: rentCarSliceState = {
   cars: [],
   status: "default",
   error: undefined,
-  selectedDates: null,
 }
 
-const CARS_URL = ""
+const CARS_URL = "/api/cars/filter"
 
-export const rentCarSlice = createAppSlice({
-  name: "RENT_CAR",
-  initialState: rentCarState,
+export const carsSlice = createAppSlice({
+  name: "cars",
+  initialState: initialCarState,
   reducers: create => ({
-    getCar: create.asyncThunk(
-      async (arg, thunkApi) => {
+    fetchCars: create.asyncThunk(
+      async (
+        filters: {
+          startDateTime: string
+          endDateTime: string
+          minPrice: number
+          maxPrice: number
+          brands: string[]
+          bodyTypes: string[]
+        },
+        thunkApi,
+      ) => {
         try {
-          const result = await axios.get(CARS_URL)
-          //check returning property
-          return result
-        } catch (error) {
-          return thunkApi.rejectWithValue(error)
+          const params = new URLSearchParams()
+          params.append("startDateTime", filters.startDateTime)
+          params.append("endDateTime", filters.endDateTime)
+          params.append("minPrice", filters.minPrice.toString())
+          params.append("maxPrice", filters.maxPrice.toString())
+          filters.brands.forEach(brand => params.append("brands", brand)),
+            filters.bodyTypes.forEach(bodyType =>
+              params.append("bodyTypes", bodyType),
+            )
+
+          // const response = await axios.get<Car[]>(CARS_URL)
+          const response = await axios.get<Car[]>(
+            `${CARS_URL}?${params.toString()}`,
+          )
+          return response.data
+        } catch (error: any) {
+          return thunkApi.rejectWithValue(error.response?.data || error.message)
         }
       },
       {
-        pending: () => {},
-        fulfilled: () => {},
-        rejected: () => {},
+        pending: (state: rentCarSliceState) => {
+          state.error = undefined
+          state.status = "loading"
+        },
+        fulfilled: (state: rentCarSliceState, action: any) => {
+          state.status = "success"
+          state.cars = action.payload
+        },
+        rejected: (state: rentCarSliceState, action: any) => {
+          state.error = action.payload || "Something went wrong..."
+          state.status = "error"
+        },
       },
     ),
   }),
-  selectors: {},
+  selectors: {
+    carsData: (state: rentCarSliceState) => state,
+  },
 })
+
+const initialBrandsState: brandsSliceState = {
+  brands: [],
+  status: "idle",
+  error: undefined,
+}
+export const brandsSlice = createAppSlice({
+  name: "brands",
+  initialState: initialBrandsState,
+  reducers: create => ({
+    fetchBrands: create.asyncThunk(
+      async (_, thunkApi) => {
+        try {
+          const response = await axios.get<string[]>("/api/cars/brands")
+          return response.data
+        } catch (error: any) {
+          return thunkApi.rejectWithValue(error.response?.data || error.message)
+        }
+      },
+      {
+        pending: (state: brandsSliceState) => {
+          state.error = undefined
+          state.status = "loading"
+        },
+        fulfilled: (state: brandsSliceState, action: any) => {
+          state.status = "success"
+          state.brands = action.payload
+        },
+        rejected: (state: brandsSliceState, action: any) => {
+          state.error = action.payload || "Something went wrong..."
+          state.status = "error"
+        },
+      },
+    ),
+  }),
+  selectors: {
+    brandsData: (state: brandsSliceState) => state.brands,
+  },
+})
+
+export const rentCarActions = carsSlice.actions
+export const rentCarSelectors = carsSlice.selectors
+
+export const brandsActions = brandsSlice.actions
+export const brandsSelectors = brandsSlice.selectors
